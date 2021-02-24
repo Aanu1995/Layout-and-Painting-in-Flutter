f8aa775caf4194082ec23b129ee8fa941bcce844
Aanu Olakunle <olakunleaanu@gmail.com>
2021-02-24 11:34:07 +0100

    Refactor the code and add screenshot

--- 1.0 customPainting/pan_to_rotate/lib/main.dart
+++ 1.0 customPainting/pan_to_rotate/lib/main.dart
@@ -13,6 +13,7 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   double degree = 0;
   double radius = 0.0;
+  final strokeWidth = 70.0;
 
   @override
   Widget build(BuildContext context) {
@@ -29,21 +30,28 @@ class _AppState extends State<App> {
                   return Stack(
                     fit: StackFit.expand,
                     children: [
-                      CustomPaint(painter: CirclePainter()),
+                      CustomPaint(
+                        painter: ColorFillerPainter(
+                          degree: degree,
+                          strokeWidth: strokeWidth,
+                        ),
+                      ),
                       GestureDetector(
                         onPanUpdate: _onPanUpdate,
                         child: CustomPaint(
-                          painter: ColorFillerPainter(degree: degree),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.all(50.0),
-                        child: AbsorbPointer(
-                          child: CustomPaint(
-                            painter: PercentagePainter(degree: degree),
+                          painter: BallPainter(
+                            degree: degree,
+                            strokeWidth: strokeWidth,
                           ),
                         ),
                       ),
+                      Container(
+                        margin: EdgeInsets.all(strokeWidth),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                        ),
+                        child: AbsorbPointer(),
+                      )
                     ],
                   );
                 },
@@ -56,118 +64,91 @@ class _AppState extends State<App> {
   }
 
   _onPanUpdate(DragUpdateDetails drag) {
-    /// Pan location on the wheel
-    bool onTop = drag.localPosition.dy <= radius;
-    bool onLeftSide = drag.localPosition.dx <= radius;
-    bool onRightSide = !onLeftSide;
-    bool onBottom = !onTop;
+    if (true) {
+      /// Pan location on the wheel
+      bool onTop = drag.localPosition.dy <= radius;
+      bool onLeftSide = drag.localPosition.dx <= radius;
+      bool onRightSide = !onLeftSide;
+      bool onBottom = !onTop;
 
-    /// Pan movements
-    bool panUp = drag.delta.dy <= 0.0;
-    bool panLeft = drag.delta.dx <= 0.0;
-    bool panRight = !panLeft;
-    bool panDown = !panUp;
+      /// Pan movements
+      bool panUp = drag.delta.dy <= 0.0;
+      bool panLeft = drag.delta.dx <= 0.0;
+      bool panRight = !panLeft;
+      bool panDown = !panUp;
 
-    /// Absolute change on axis
-    double yChange = drag.delta.dy.abs();
-    double xChange = drag.delta.dx.abs();
+      /// Absolute change on axis
+      double yChange = drag.delta.dy.abs();
+      double xChange = drag.delta.dx.abs();
 
-    /// Directional change on wheel
-    double verticalRotation = (onRightSide && panDown) || (onLeftSide && panUp)
-        ? yChange
-        : yChange * -1;
+      /// Directional change on wheel
+      double verticalRotation =
+          (onRightSide && panDown) || (onLeftSide && panUp)
+              ? yChange
+              : yChange * -1;
 
-    double horizontalRotation =
-        (onTop && panRight) || (onBottom && panLeft) ? xChange : xChange * -1;
+      double horizontalRotation =
+          (onTop && panRight) || (onBottom && panLeft) ? xChange : xChange * -1;
 
 // Total computed change
-    double rotationalChange = verticalRotation + horizontalRotation;
-    // double _value = degree + rotationalChange; // correct value to use
+      double rotationalChange = verticalRotation + horizontalRotation;
+      // double _value = degree + rotationalChange; // correct value to use
 
-    // use to slow down 5 times when pan
-    double _value = degree + (rotationalChange / 5);
-    setState(() {
-      // convert to degree of rotation
-      degree = _value <= 0
-          ? 0
-          : _value >= 360
-              ? 360
-              : _value;
-    });
-  }
-}
-
-class CirclePainter extends CustomPainter {
-  const CirclePainter();
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final radius = size.width / 2;
-    canvas.save();
-    canvas.translate(radius, radius);
-
-    Paint paint = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 3.0
-      ..color = Colors.green;
-
-    // paint the outside circle
-    canvas.drawCircle(Offset(0, 0), radius, paint);
-    // paint the inner circle
-    canvas.drawCircle(Offset(0, 0), radius - 50, paint);
-    canvas.restore();
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
+      // use to slow down 5 times when pan
+      double _value = degree + (rotationalChange / 5);
+      setState(() {
+        // convert to degree of rotation
+        degree = _value <= 0
+            ? 0
+            : _value >= 360
+                ? 360
+                : _value;
+      });
+    }
   }
 }
 
 class ColorFillerPainter extends CustomPainter {
   final double degree;
-  ColorFillerPainter({this.degree});
+  final strokeWidth;
+  ColorFillerPainter({this.degree, this.strokeWidth});
 
   @override
   void paint(Canvas canvas, Size size) {
     final radius = size.width / 2;
+    final bigStrokeWidth = strokeWidth;
+    final smallStrokeWidth = strokeWidth - 10;
+
+    Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = bigStrokeWidth
+      ..color = Colors.green;
+
+    Paint smallCirclePaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = smallStrokeWidth
+      ..color = Colors.white;
 
     canvas.save();
     canvas.translate(radius, radius);
+
+    final newRadius = radius - (bigStrokeWidth / 2);
+
+    // paint the outside circle
+    canvas.drawCircle(Offset(0, 0), newRadius, paint);
+    // paint the inner circle
+    canvas.drawCircle(Offset(0, 0), newRadius, smallCirclePaint);
+
     final angle = (degree * 2 * pi) / 360;
+
+    canvas.save();
 
     canvas.rotate(-pi / 2);
 
-    Paint paint2 = Paint()
-      ..style = PaintingStyle.fill
-      ..color = Colors.green;
-    canvas.drawArc(Rect.fromCircle(center: Offset(0, 0), radius: radius), 0,
-        angle, true, paint2);
+    canvas.drawArc(Rect.fromCircle(center: Offset(0, 0), radius: newRadius), 0,
+        angle, false, paint);
 
     canvas.restore();
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
-  }
-}
-
-class PercentagePainter extends CustomPainter {
-  final double degree;
-  PercentagePainter({this.degree});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final radius = size.width / 2;
-
-    canvas.save();
-    canvas.translate(radius, radius);
-
-    Paint paint3 = Paint()
-      ..style = PaintingStyle.fill
-      ..color = Colors.white;
-    canvas.drawCircle(Offset(0, 0), radius - 2, paint3);
 
     final textPainter = TextPainter(
         textAlign: TextAlign.center, textDirection: TextDirection.ltr);
@@ -184,6 +165,45 @@ class PercentagePainter extends CustomPainter {
 
     textPainter.paint(canvas,
         Offset(-(textPainter.width / 2.0), -(textPainter.height / 2.0)));
+
+    canvas.restore();
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class BallPainter extends CustomPainter {
+  final double degree;
+  final strokeWidth;
+  BallPainter({this.degree, this.strokeWidth});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final radius = size.width / 2;
+
+    canvas.save();
+    canvas.translate(radius, radius);
+
+    final angle = (degree * 2 * pi) / 360;
+
+    canvas.rotate(angle);
+
+    Paint paint = Paint()
+      ..style = PaintingStyle.fill
+      ..strokeCap = StrokeCap.round
+      ..color = Colors.green;
+    Paint paint1 = Paint()
+      ..style = PaintingStyle.fill
+      ..strokeCap = StrokeCap.round
+      ..color = Colors.white;
+
+    final circleRadius = strokeWidth / 2;
+    canvas.drawCircle(Offset(0, -radius + circleRadius), circleRadius, paint1);
+    canvas.drawCircle(Offset(10, -radius + circleRadius), circleRadius, paint);
+
     canvas.restore();
   }
 
